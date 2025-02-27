f855f125481283a0287315049a109ba6864690c9
moihm96 <moihermol@gmail.com>
2025-02-27 12:31:49 +0000

    "Fix GitHub Pages base path issue"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/moihm96/frontend-reto-reactjs",
+  base: "/frontend-reto-reactjs/",
   build: {
     outDir: "dist",
   },
