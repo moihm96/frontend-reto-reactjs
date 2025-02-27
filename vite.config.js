7c2e53ee4dba02f238ab77e78999bec2704e790b
moihm96 <moihermol@gmail.com>
2025-02-27 12:17:02 +0000

    Changing configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://github.com/moihm96/frontend-reto-reactjs",
+  build: {
+    outDir: "dist",
+  },
+});
